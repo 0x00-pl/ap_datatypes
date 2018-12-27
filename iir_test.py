c61ac58cf5afa9e2d8e42b0a525b1b7634fe3366
0x00-pl <0x00.pl@gmail.com>
2018-12-27 20:11:05 +0800

    test chirp

--- iir_test.py
+++ iir_test.py
@@ -1,7 +1,13 @@
+import random
+
 import math
 
+import audio_export
 from ac_datatype import AcFixed
 import monitor
+
+iir3_m_pa = monitor.ErrorMonitor('iir3_m_pa')
+iir3_m_pb = monitor.ErrorMonitor('iir3_m_pb')
 
 iir3_m_r1 = monitor.ErrorMonitor('irr3_m_r1')
 iir3_m_t1 = monitor.ErrorMonitor('irr3_m_t1')
@@ -9,6 +15,10 @@ iir3_m_r2 = monitor.ErrorMonitor('irr3_m_r2')
 iir3_m_t2 = monitor.ErrorMonitor('irr3_m_t2')
 iir3_m_r3 = monitor.ErrorMonitor('irr3_m_r3')
 iir3_m_t3 = monitor.ErrorMonitor('irr3_m_t3')
+iir3_m_r4 = monitor.ErrorMonitor('irr3_m_r4')
+iir3_m_t4 = monitor.ErrorMonitor('irr3_m_t4')
+iir3_m_r5 = monitor.ErrorMonitor('irr3_m_r5')
+iir3_m_t5 = monitor.ErrorMonitor('irr3_m_t5')
 iir7_m_r = monitor.ErrorMonitor('irr7_m_r')
 iir7_m_t = monitor.ErrorMonitor('irr7_m_t')
 
@@ -54,22 +64,62 @@ def apply_iir3_m(seq, b3, a3, arg_pow, mr=None, mt=None):
         reg2 = b3[2] * i - a3[2] * r
         reg2 = reg2.to_fixed(16, 1, True, AcFixed.QuantizationMode.RND, AcFixed.OverflowMode.SAT, mr)
 
-        r = t
         ret.append(r)
 
     return ret
 
 
 def test_iir3x3(seq):
-    b3 = [0.982, -1.964, 0.982]
-    b3 = [i / 1.3 for i in b3]
-    a3 = [1, -1.964, 0.9646]
+    # b3 = [0.982, -1.964, 0.982]
+    # b3 = [i / 1.3 for i in b3]
+    # a3 = [1, -1.964, 0.9646]
+    # sos = [
+    #     [1, - 1.99993896484375, 1, 1, - 1.99774169921875, 0.997863769531250],
+    #     [1, - 1.99993896484375, 1, 1, - 1.99145507812500, 0.991638183593750],
+    #     [1, - 2, 1, 1,                - 1.97668457031250, 0.976989746093750],
+    #     [1, - 1, 0, 1,                - 0.979125976562500, 0]
+    # ]
+    # g = [
+    #     0.620239257812500,
+    #     0.995788574218750,
+    #     0.988403320312500,
+    #     1.59375
+    # ]
+    sos = [
+        [1, - 1.99993896484375, 1, 1, - 1.99768066406250, 0.997802734375000],
+        [1, - 2, 1, 1, - 1.99127197265625, 0.991455078125000],
+        [1, - 2, 1, 1, - 1.97631835937500, 0.976623535156250],
+        [1, - 1, 0, 1, - 0.978881835937500, 0]
+    ]
+    g = [
+        0.612609863281250,
+        0.995666503906250,
+        0.988220214843750,
+        1.61322021484375
+    ]
+    a = []
+    b = []
+    for ab, ig in zip(sos, g):
+        b3 = ab[:3]
+        b3 = [i * ig for i in b3]
+        a3 = ab[3:]
+        b.append(b3)
+        a.append(a3)
+
     arg_pow = math.ceil(math.log2(max(1, *[abs(i) for i in a3], *[abs(i) for i in b3])))
 
-    r1 = apply_iir3_m(seq, b3, a3, arg_pow, iir3_m_r1, iir3_m_t1)
-    r1 = apply_iir3_m(r1, [1.3, 0, 0], [1, 0, 0], arg_pow, iir3_m_r2, iir3_m_t2)
-    r1 = apply_iir3_m(r1, b3, a3, arg_pow, iir3_m_r3, iir3_m_t3)
-    r1 = apply_iir3_m(r1, [1.3, 0, 0], [1, 0, 0], arg_pow, iir3_m_r3, iir3_m_t3)
+    # print('min eng', eng(seq))
+    r1 = apply_iir3_m(seq, b[0], a[0], 1, iir3_m_r1, iir3_m_t1)
+    # print('min eng', eng(r1))
+    r1 = apply_iir3_m(r1, b[1], a[1], 1, iir3_m_r2, iir3_m_t2)
+    # print('min eng', eng(r1))
+    r1 = apply_iir3_m(r1, b[2], a[2], 1, iir3_m_r3, iir3_m_t3)
+    # print('min eng', eng(r1))
+    r1 = apply_iir3_m(r1, b[3], a[3], 1, iir3_m_r4, iir3_m_t4)
+    # print('min eng', eng(r1))
+    # r1 = apply_iir3_m(r1, [2, 0, 0], [1, 0, 0], 2, iir3_m_r5, iir3_m_t5)
+    # r1 = apply_iir3_m(r1, b3, a3, arg_pow, iir3_m_r3, iir3_m_t3)
+    # r1 = apply_iir3_m(r1, [1.3, 0, 0], [1, 0, 0], arg_pow, iir3_m_r3, iir3_m_t3)
     # r1 = apply_iir3_m(r1, b3, a3, arg_pow, iir3_m_r3, iir3_m_t3)
     # r1 = apply_iir3_m(r1, [1.3, 0, 0], [1, 0, 0], arg_pow, iir3_m_r2, iir3_m_t2)
     # r1 = apply_iir3_m(r1, b3, a3, arg_pow, iir3_m_r3, iir3_m_t3)
@@ -131,7 +181,14 @@ def eng(seq):
 #
 
 def test_all_freq():
-    for freq in range(0, 24000, 100):
+    for freq in range(10, 200, 10):
+        seq = [math.sin(i * 2 * math.pi / 48000 * freq) for i in range(1000)]
+        seq = [AcFixed(16, 1, True, i) for i in seq]
+        print('seq {} err db'.format(freq), err_sqr_db(seq))
+        r1 = test_iir3x3(seq)
+        print('ret {} eng'.format(freq), eng(r1), eng(seq))
+
+    for freq in range(200, 24000, 100):
         seq = [math.sin(i * 2 * math.pi / 48000 * freq) for i in range(1000)]
         seq = [AcFixed(16, 1, True, i) for i in seq]
         print('seq {} err db'.format(freq), err_sqr_db(seq))
@@ -140,12 +197,14 @@ def test_all_freq():
 
 
 if __name__ == '__main__':
-    test_all_freq()
-    exit()
-    seq = [i * 2 * math.pi / 48000 * 400 for i in range(4800)]
-    seq = [math.sin(i) for i in seq]
-    # seq = [1] * len(seq)
-    seq = [AcFixed(16, 1, True, i) for i in seq]
+    # test_all_freq()
+    # exit()
+    seq = [i * 2 * math.pi / 48000 * 150 for i in range(4800)]
+    seq = [math.sin(i)/4 for i in seq]
+    #seq = [1] * len(seq)
+    # seq = [1e-4*(random.random()-0.5) for i in seq]
+    seq = audio_export.data
+    seq = [AcFixed(16, 1, True, i/2+0.5) for i in seq]
 
     print('seq err db', err_sqr_db(seq))
     r1 = test_iir3x3(seq)
@@ -159,11 +218,15 @@ if __name__ == '__main__':
     print('ret values:', [i for i in r1[1100:1200]])
     # print([i for i in r2[1100:1200]])
     print()
-    print(iir3_m_r1)
     print(iir3_m_t1)
-    print(iir3_m_r2)
+    print(iir3_m_r1)
     print(iir3_m_t2)
-    print(iir3_m_r3)
+    print(iir3_m_r2)
     print(iir3_m_t3)
+    print(iir3_m_r3)
+    print(iir3_m_t4)
+    print(iir3_m_r4)
+    print(iir3_m_t5)
+    print(iir3_m_r5)
     # print(iir7_m_r)
     # print(iir7_m_t)
--- monitor.py
+++ monitor.py
@@ -10,8 +10,8 @@ class ErrorMonitor:
         self.minv, self.maxv = None, None
 
     def __call__(self, before_v, after_v, golden_v, reason='any'):
-        self.minv = min(self.minv, golden_v) if self.minv is not None else golden_v
-        self.maxv = max(self.maxv, golden_v) if self.maxv is not None else golden_v
+        self.minv = min(self.minv, before_v) if self.minv is not None else before_v
+        self.maxv = max(self.maxv, before_v) if self.maxv is not None else before_v
         self.count = self.count + 1
         if reason == 'quantization':
             self.err_quantization_inc = self.err_quantization_inc + \
